234ecf859d7d969498ba141c1c7b9e3515874cf0
marijn111 <39378193+marijn111@users.noreply.github.com>
2020-08-31 00:56:38 +0200

    Update settings.py

--- the_wheather/the_wheather/settings.py
+++ the_wheather/the_wheather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mx(@4&0%r%^9+5+++nf4*@-yhfavlcyqj7^l2q_rlbjf$n@rdd'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
